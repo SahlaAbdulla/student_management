eabc071550fd512d356470fa224a2fe40b64674f
SahlaAbdulla <abdullasahla7@gmail.com>
2026-03-26 10:06:08 +0530

    changed database to sqlite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.mysql',
- 'NAME': 'studentdb',
- 'USER': 'root',
- 'PASSWORD': 'password@123',
- 'HOST': 'localhost',
- 'PORT': '3306',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
