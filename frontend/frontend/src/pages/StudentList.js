d4b17949a240cb49975c355a9ecf31ea4deea0ad
SahlaAbdulla <abdullasahla7@gmail.com>
2026-03-26 11:55:13 +0530

    changed

--- frontend/frontend/src/pages/StudentList.js
+++ frontend/frontend/src/pages/StudentList.js
@@ -1,90 +1,34 @@
-import { useEffect,useState } from "react";
-
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import API from "../api";
 
 function StudentList(){
 
- const [students,setStudents] = useState([]);
+  const [students, setStudents] = useState([]);
 
- useEffect(()=>{
-   loadStudents();
- },[]);
+  useEffect(() => {
+    API.get("students/")
+      .then(res => {
+        console.log("DATA:", res.data);
 
- const loadStudents = ()=>{
-   API.get("students/")   // ✅ changed
-   .then(res=>{
-     setStudents(res.data);
-   });
- };
+        // 🔥 FIX HERE
+        const data = Array.isArray(res.data) ? res.data : [res.data];
+        setStudents(data);
+      });
+  }, []);
 
- const deleteStudent = (id)=>{
+  return (
+    <div className="container">
 
-   API.delete(`students/${id}/`)  
-   .then(()=>{
-    //  alert("Student Deleted");
-     loadStudents();
-   });
+      <h2>Students</h2>
 
- };
+      {students.map((s) => (
+        <div key={s.id}>
+          {s.name} - {s.email} - {s.course}
+        </div>
+      ))}
 
- return(
-
- <div className="container">
-
- <h2 className="mt-4">Student List</h2>
-
- <table className="table table-bordered">
-
- <thead className="table-dark">
-
- <tr>
- <th>ID</th>
- <th>Name</th>
- <th>Email</th>
- <th>Course</th>
- <th>Action</th>
- </tr>
-
- </thead>
-
- <tbody>
-
- {students.map(student=>(
-   <tr key={student.id}>
-
-   <td>{student.id}</td>
-   <td>{student.name}</td>
-   <td>{student.email}</td>
-   <td>{student.course}</td>
-
-   <td>
-
-   <Link
-   className="btn btn-warning me-2"
-   to={`/edit/${student.id}`}>
-   Edit
-   </Link>
-
-   <button
-   className="btn btn-danger"
-   onClick={()=>deleteStudent(student.id)}>
-   Delete
-   </button>
-
-   </td>
-
-   </tr>
- ))}
-
- </tbody>
-
- </table>
-
- </div>
-
- );
-
+    </div>
+  );
 }
 
 export default StudentList;
